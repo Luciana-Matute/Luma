ad893270180e3f07c83d6aa66a6b6140162928f5
Luciana Matute <luciana@MacBook-Pro-de-Valeria-Ghezzi-2.local>
2016-08-25 12:59:45 -0500

    en proceso falta arreglar

--- js/main.js
+++ js/main.js
@@ -1,94 +1,52 @@
-// Porcentajes circulares
-(function() {
-
-var proto = $.circleProgress.defaults,
-    superInitWidget = proto.initWidget,
-    superDrawFrame = proto.drawFrame;
-
-$.extend(proto, {
-    initWidget: function() {
-        superInitWidget.call(this);
-        this.dotOffset = this.dotRadius - this.getThickness() / 2;
-        this.radius -= this.dotOffset;
-    },
-    
-    drawFrame: function(v) {
-        this.ctx.save();
-        this.ctx.clearRect(0, 0, this.size, this.size);
-        this.ctx.translate(this.dotOffset, this.dotOffset);
-        superDrawFrame.call(this, v);
-        this.drawDot();
-        this.ctx.restore();
-    },
-    
-    drawDot: function() {
-        var ctx = this.ctx,
-            sa = this.startAngle,
-            r = this.radius,
-            rd = r - this.getThickness() / 2,
-            x = r + rd * Math.cos(sa),
-            y = r + rd * Math.sin(sa);
-
-        ctx.save();
-        ctx.fillStyle = this.arcFill;
-        ctx.beginPath();
-        ctx.arc(x, y, this.dotRadius, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.restore();
-    }
+// Closes the Responsive Menu on Menu Item Click
+$ ('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
 });
-    
-})();
-
-var createObjects = false;
-$(window).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y >= 1000) { //depende de cuánto mida mi página de largo en px
-        if (!createObjects) {
-            createObjects = true;
-            $('.circle').circleProgress({
-            dotRadius: 12, // new attribute
-            size: 160,
-            thickness: 15,
-            startAngle: -Math.PI / 2,
-            fill: {
-                gradient: ['#EDA2CC', '#495F8C']
-            }
-        });
-
-        $('.1').circleProgress('value', 0.93);
-        $('.2').circleProgress('value', 0.92);
-        $('.3').circleProgress('value', 0.9);
-        $('.4').circleProgress('value', 0.88);
-        $('.5').circleProgress('value', 0.95);
-        $('.6').circleProgress('value', 0.5);
-        $('.7').circleProgress('value', 0.8);
+$(document).ready(function(){
+    loadGallery(true, 'a.img-portfolio');
+    //This function disables buttons when needed
+    function disableButtons(counter_max, counter_current){
+        $('#show-previous-image, #show-next-image').show();
+        if(counter_max == counter_current){
+            $('#show-next-image').hide();
+        } else if (counter_current == 1){
+            $('#show-previous-image').hide();
         }
     }
+    /**
+     *
+     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+     * @param setClickAttr  Sets the attribute for the click handler.
+     */
+    function loadGallery(setIDs, setClickAttr){
+        var current_image,
+            selector,
+            counter = 0;
+        $('#show-next-image, #show-previous-image').click(function(){
+            if($(this).attr('id') == 'show-previous-image'){
+                current_image--;
+            } else {
+                current_image++;
+            }
+            selector = $('[data-image-id="' + current_image + '"]');
+            updateGallery(selector);
+        });
+        function updateGallery(selector) {
+            var $sel = selector;
+            current_image = $sel.data('image-id');
+            $('#image-gallery-caption').text($sel.data('caption'));
+            $('#image-gallery-title').text($sel.data('title'));
+            $('#image-gallery-image').attr('src', $sel.data('image'));
+            disableButtons(counter, $sel.data('image-id'));
+        }
+        if(setIDs == true){
+            $('[data-image-id]').each(function(){
+                counter++;
+                $(this).attr('data-image-id',counter);
+            });
+        }
+        $(setClickAttr).on('click',function(){
+            updateGallery($(this));
+        });
+    }
 });
-
-
-$(".circle").smoove({
-    offset  : '15%',
-    moveY   : '100px'
-});
-$(".nav-tabs").smoove({
-    offset  : '15%',
-    moveY   : '100px'
-});
-$(".tab-content").smoove({
-    offset  : '15%',
-    moveY   : '100px'
-});
-
-//About me
-$(".about-text").smoove({
-    offset  : '15%',
-    moveY   : '100px'
-});
-$(".about-title").smoove({
-    offset  : '15%',
-    moveY   : '100px'
-});
-
-//Desplazamiento lento de la página
