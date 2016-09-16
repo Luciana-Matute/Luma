e2ae35aefead32441af3fb801a6da4b5ed7a937f
Luciana Matute <luciana@MacBook-Pro-de-Valeria-Ghezzi-2.local>
2016-09-16 13:00:48 -0500

    arreglos en destok falta movil

--- js/main.js
+++ js/main.js
@@ -13,6 +13,51 @@ $(document).ready(function(){
             $('#show-previous-image').hide();
         }
     }
+
+//
+  // Intercepta clicks en menu principal para hacer scroll animado hasta la
+  // sección correspondiente.
+  //
+  $(document).on('click', '.navbar a', function (e) {
+
+    var href = $(e.currentTarget).attr('href');
+
+    // We only care about "fragments"...
+    if (href.charAt(0) !== '#') {
+      return;
+    }
+
+    $('html, body').animate({
+      scrollTop: $(href).offset().top
+    }, 1500, 'swing');
+
+    return false;
+  });
+
+
+  //
+  // Update location hash as we scroll...
+  //
+  if (window.history && window.history.pushState) {
+    $(document).on('scroll', function () {
+
+      $('article').each(function () {
+
+        // if begins before top but ends in visible area
+        //+ 10 allows you to change hash before it hits the top border
+        var beginsBeforeTop = $(this).offset().top < window.pageYOffset + 10;
+        var endsInVisibleArea = $(this).offset().top + $(this).height() > window.pageYOffset + 10;
+        if (beginsBeforeTop && endsInVisibleArea) {
+          var id = $(this).attr('id');
+          window.history.pushState({}, id, '#' + id);
+        }
+      });
+    });
+  }
+
+
+
+    
     /**
      *
      * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
